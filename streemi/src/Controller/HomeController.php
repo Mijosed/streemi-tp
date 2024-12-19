7f07fe60cf18b257d7a207f51b47641496a8c011
Mijosé Difuidi <mijosed@gmail.com>
2024-12-19 01:21:12 +0100

    un peu de dynamisme

--- streemi/src/Controller/HomeController.php
+++ streemi/src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\MediaRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,8 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route(path:'/', name: 'page_home')]
-    public function home(): Response
+    public function home(MediaRepository $mediaRepository
+    ): Response
     {
-        return $this->render(view: 'index.html.twig');
+        $medias = $mediaRepository->findAll();
+
+        return $this->render( 'index.html.twig', ['medias' => $medias]);
     }
 }   
--- streemi/src/Controller/Movie/MovieController.php
+++ streemi/src/Controller/Movie/MovieController.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller\Movie;
 
+use App\Entity\Media;
+use App\Repository\MediaRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MovieController extends AbstractController
 {
-    #[Route(path:'/movie', name: 'page_detail_movie')]
-    public function detail(): Response
+    #[Route(path:'/movie/{id}', name: 'page_detail_movie')]
+    public function detail(
+        Media $medias
+    ): Response
     {
-        return $this->render(view: 'movie/detail.html.twig');
+        return $this->render('movie/detail.html.twig', ['medias' => $medias]);
     }
     
     //serie
